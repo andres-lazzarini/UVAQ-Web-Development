46e33d1eec862bc9c18ea6f5f8c7935a5d553c93
AndresLazzarini <andres.lazzarini@urvenue.com>
2022-11-01 19:54:58 -0600

    updates

---
+++ Angular/my-first-project/src/app/_global/global.module.ts
@@ -0,0 +1,19 @@
+// import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { HeaderComponent } from './_components/header/header.component';
+// import { FooterComponent } from './_components/footer/footer.component';
+// import { SidebarComponent } from './_components/sidebar/sidebar.component';
+// import { ContentComponent } from './_components/content/content.component';
+
+// @NgModule({
+//   declarations: [
+//     HeaderComponent,
+//     FooterComponent,
+//     SidebarComponent,
+//     ContentComponent
+//   ],
+//   imports: [
+//     CommonModule
+//   ]
+// })
+// export class GlobalModule { }
--- Angular/my-first-project/src/app/app.module.ts
+++ Angular/my-first-project/src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
 import { PruebaComponent } from './prueba/prueba.component';
+import { HeaderComponent } from './_global/_components/header/header.component';
+import { FooterComponent } from './_global/_components/footer/footer.component';
+import { SidebarComponent } from './_global/_components/sidebar/sidebar.component';
+import { ContentComponent } from './_global/_components/content/content.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PruebaComponent
+    PruebaComponent,
+    HeaderComponent,
+    SidebarComponent,
+    ContentComponent,
+    FooterComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
